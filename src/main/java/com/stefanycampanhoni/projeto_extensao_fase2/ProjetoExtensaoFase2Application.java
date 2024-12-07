2697bd1b446226dcbf4f2e878428b3c6d78046a7
Stefany <scampanhoni@gmail.com>
2024-12-07 09:35:57 -0300

    Inital commit

---
+++ src/main/java/com/stefanycampanhoni/projeto_extensao_fase2/ProjetoExtensaoFase2Application.java
@@ -0,0 +1,13 @@
+package com.stefanycampanhoni.projeto_extensao_fase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoExtensaoFase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoExtensaoFase2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefanycampanhoni/projeto_extensao_fase2/ProjetoExtensaoFase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanycampanhoni.projeto_extensao_fase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoExtensaoFase2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
